de87a825a638bb9780043fa5c45be61b8d0996bc
Jenny Johnson <kippz07@users.noreply.github.com>
2017-06-16 15:28:33 +0100

    added switch solution

--- main.js
+++ main.js
@@ -10,11 +10,30 @@
 
 // }
 
-for (var i = 1; i <= 100; i++) {
+// for (var i = 1; i <= 100; i++) {
 
-	(i % 3 === 0) && (i % 5 === 0) ? console.log('fizzbuzz') :
-		((i % 3 === 0) ? console.log('fizz') :
-				((i % 5 === 0)) ? console.log('buzz') : console.log(i));
+// 	(i % 3 === 0) && (i % 5 === 0) ? console.log('fizzbuzz') 
+// 		: ((i % 3 === 0) ? console.log('fizz')
+// 			: (i % 5 === 0) ? console.log('buzz') 
+// 				: console.log(i));
 
+
+// }
+
+for (var i = 1; i <= 100; i++){
+
+	switch (true){
+
+		case (i % 3 === 0) && (i % 5 === 0): console.log('fizzbuzz');
+		break;
+
+		case (i % 3 === 0): console.log('fizz');
+		break;
+
+		case (i % 5 === 0): console.log('buzz');
+		break;
+
+		default: console.log(i);
+	}
 
 }
