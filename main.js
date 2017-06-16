126a6c136f7eae92100d5e2ba5b126b826886598
Jenny Johnson <kippz07@users.noreply.github.com>
2017-06-16 15:20:25 +0100

    included ternary solution

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+// for (var i = 1; i <= 100; i++) {
+
+// 	if ((i % 3 === 0) && (i % 5 === 0)) {
+// 		console.log('fizzbuzz');
+// 	} else if (i % 3 === 0) {
+// 		console.log('fizz');
+// 	} else if (i % 5 === 0) {
+// 		console.log('buzz');
+// 	} else console.log(i);
+
+// }
+
 for (var i = 1; i <= 100; i++) {
 
-	if ((i % 3 === 0) && (i % 5 === 0)) {
-		console.log('fizzbuzz');
-	} else if (i % 3 === 0) {
-		console.log('fizz');
-	} else if (i % 5 === 0) {
-		console.log('buzz');
-	} else console.log(i);
+	(i % 3 === 0) && (i % 5 === 0) ? console.log('fizzbuzz') :
+		((i % 3 === 0) ? console.log('fizz') :
+				((i % 5 === 0)) ? console.log('buzz') : console.log(i));
+
 
 }
